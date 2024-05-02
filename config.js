eff24f9be7fccd5555dadb3f8b77b920120a9756
Duke <94008471+DuckMei@users.noreply.github.com>
2024-05-02 06:25:31 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  channel: "1235222113301037086",
+  owner: "797345693031071774",
+  disable: {
+    hunt: false,
+    battle: false,
+    owo: false,
+    buy: true,
+    pray: false,
+  },
+  inv: [54,55,56]
+}
