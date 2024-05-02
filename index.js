a64b89fa82b14549eae8c474932bf89f736858ef
Duke <94008471+DuckMei@users.noreply.github.com>
2024-05-02 06:24:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,130 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({
+  checkUpdate: false,
+});
+const data = require("./config.js");
+const { scheduleJob, RecurrenceRule } = require("node-schedule");
+let channel = data.channel;
+let owner = data.owner;
+const huntBattleRule = new RecurrenceRule();
+huntBattleRule.second = [0, 36];
+const buyRule = new RecurrenceRule();
+buyRule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+const owoRule = new RecurrenceRule();
+owoRule.second = [0, 29, 52];
+const prayRule = new RecurrenceRule();
+prayRule.minute = [0, 6]
+
+let huntJob;
+let buyJob;
+let owoJob;
+let prayJob;
+
+client.on('ready', async () => {
+  if (!data.disable.hunt || !data.disable.battle)
+    huntJob = scheduleJob(huntBattleRule, () => {
+      setTimeout(() => {
+        if (!data.disable.hunt) client.channels.cache.get(channel).send("oh");
+      }, 1500);
+      setTimeout(() => {
+        if (!data.disable.battle)
+          client.channels.cache.get(channel).send("ob");
+      }, 1800);
+    });
+
+  buyJob = scheduleJob(buyRule, () => {
+    if (!data.disable.buy) client.channels.cache.get(channel).send("owobuy 1");
+  });
+  owoJob = scheduleJob(owoRule, () => {
+    if (!data.disable.owo) client.channels.cache.get(channel).send("owo");
+  });
+  prayJob = scheduleJob(prayRule, () => {
+    if (!data.disable.pray) client.channels.cache.get(channel).send("opray");
+  });
+  console.log(`${client.user.username} is ready!`);
+})
+
+client.on("messageCreate", async message => {
+  if (message.channel.id !== channel && message.channel.type !== "dm") return;
+  if (message.author.id !== "408785106942164992") return;
+
+  if (
+    [
+      "Beep Boop. A",
+      "real human?",
+      "can check!",
+      "Please DM me",
+      "Wrong verification",
+      " Please complete your captcha",
+      "solving the captcha",
+      "http://verify.owobot.com/",
+      " Please use the link ",
+    ].some((phrase) =>
+      message.content.toLowerCase().includes(phrase.toLowerCase())
+    )
+  ) {
+    buyJob.cancel();
+    huntJob.cancel();
+    owoJob.cancel();
+    prayJob.cancel();
+  }
+  
+  if (message.content.includes("Thank you! :3")) {
+    buyJob.reschedule(buyRule);
+    huntJob.reschedule(huntBattleRule);
+    owoJob.reschedule(owoRule);
+    prayJob.reschedule(prayRule);
+  }
+      
+  if (
+    ["spent 5", "and caught an"].some((phrase) =>
+      message.content.toLowerCase().includes(phrase.toLowerCase())
+    )
+  ) {
+    //stop
+    huntJob.cancel();
+    buyJob.cancel();
+    owoJob.cancel();
+    prayJob.cancel();
+
+    let randomGem = getRandomInt(data.inv.length);
+    setTimeout(() => {
+      client.channels.cache
+        .get(channel)
+        .send(
+          "owouse " +
+            data.inv[randomGem].toString() +
+            " " +
+            (data.inv[randomGem] + 14).toString() +
+            " " +
+            (data.inv[randomGem] + 21).toString()
+        );
+    }, 1686);
+    buyJob.reschedule(buyRule);
+    huntJob.reschedule(huntBattleRule);
+    owoJob.reschedule(owoRule);
+    prayJob.reschedule(prayRule);
+  }
+})
+client.on("messageCreate", async message => {
+  if (message.channel.id === channel && message.author.id === owner) {
+    if (message.content.toLowerCase() === "dmof") {
+      huntJob.cancel();
+      buyJob.cancel();
+      owoJob.cancel();
+      prayJob.cancel();
+    }
+    if (message.content.toLowerCase() === "dmon") {
+      buyJob.reschedule(buyRule);
+      huntJob.reschedule(huntBattleRule);
+      owoJob.reschedule(owoRule);
+      prayJob.reschedule(prayRule);
+    }
+  }
+}) 
+
+client.login(process.env.token);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
